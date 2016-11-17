44299cbd66b8d4527f66c676d944abc85befd39b
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2016-11-17 12:48:36 +0000

    code cleaning

--- modules/timezone.R
+++ modules/timezone.R
@@ -3,13 +3,8 @@ timezoneUI <- function(id) {
   ns <- NS(id)
   
   tags$div(id = ns("timezone"), 
-           width = "100%", height = "100%",
-           style="overflow: hidden !important;",
     tags$iframe(src = "https://timezone.io/team/rstudio", 
                 frameborder="0", 
-                #style="overflow: hidden; height: 100%; width: 100%; position: absolute;", 
-                style="overflow: hidden !important;",
-                #scrolling="no",
                 height = "800px",
                 width = "100%")
   )
